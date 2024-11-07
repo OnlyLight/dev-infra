a96ee39bfc6e21bbb3a7e184a6cc6b14b484e1e9
duytran <duyquangtran@kms-technology.com>
2024-11-07 03:06:46 -0600

    Add Blue-Green deployment Strategy

--- helm/install.sh
+++ helm/install.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
-kubectl apply -f storageclass.yaml
+# kubectl apply -f storageclass.yaml
 
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.1/cert-manager.yaml
-# kubectl apply -f issuer.yaml
-# kubectl apply -f issuer.yaml -n argocd
+# kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.1/cert-manager.yaml
+# # kubectl apply -f issuer.yaml
+# # kubectl apply -f issuer.yaml -n argocd
 
 cd application/
 helm install postgres postgres/
@@ -16,4 +16,5 @@ helm install migrate-init migrate-init/
 helm install migrate migrate/
 helm install consumer consumer/
 
-helm install nginx-ingress ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace
+# kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace
