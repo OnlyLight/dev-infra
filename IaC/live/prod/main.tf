7c9442de323a92d064b20ecb134c380e71c657e7
duytran <duyquangtran@kms-technology.com>
2024-12-02 02:13:05 -0600

    add prod environment

--- IaC/live/prod/main.tf
+++ IaC/live/prod/main.tf
@@ -1,13 +1,13 @@
 module "vpc" {
   source = "git@github-win:OnlyLight/dev-infra.git//IaC/modules/vpc?ref=v0.0.1"
 
-  env = "dev"
+  env = "prod"
 }
 
 module "eks" {
   source = "git@github-win:OnlyLight/dev-infra.git//IaC/modules/eks?ref=v0.0.1"
 
-  env = "dev"
+  env = "prod"
   private_zone1_id = module.vpc.private_zone1_id
   private_zone2_id = module.vpc.private_zone2_id
 }
