845e2c8daec086127226b6b20480eaa3ecd9420c
duytran <duyquangtran@kms-technology.com>
2025-05-22 15:37:13 +0700

    modify resource terrform

--- IaC/modules/eks/6_output.tf
+++ IaC/modules/eks/6_output.tf
@@ -1,17 +1,13 @@
 output "cluster_name" {
-  value = aws_eks_cluster.main.name
+  value = aws_eks_cluster.eks.name
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.main.endpoint
+  value = aws_eks_cluster.eks.endpoint
 }
 
 output "cluster_ca_certificate" {
-  value = aws_eks_cluster.main.certificate_authority[0].data
-}
-
-data "aws_eks_cluster_auth" "main" {
-  name = module.eks.cluster_name
+  value = aws_eks_cluster.eks.certificate_authority[0].data
 }
 
 output "kubeconfig" {
@@ -19,19 +15,19 @@ output "kubeconfig" {
 apiVersion: v1
 clusters:
 - cluster:
-    server: ${module.eks.cluster_endpoint}
-    certificate-authority-data: ${module.eks.cluster_ca_certificate}
-  name: ${module.eks.cluster_name}
+    server: ${aws_eks_cluster.eks.endpoint}
+    certificate-authority-data: ${aws_eks_cluster.eks.certificate_authority[0].data}
+  name: ${aws_eks_cluster.eks.name}
 contexts:
 - context:
-    cluster: ${module.eks.cluster_name}
-    user: ${module.eks.cluster_name}
-  name: ${module.eks.cluster_name}
-current-context: ${module.eks.cluster_name}
+    cluster: ${aws_eks_cluster.eks.name}
+    user: ${aws_eks_cluster.eks.name}
+  name: ${aws_eks_cluster.eks.name}
+current-context: ${aws_eks_cluster.eks.name}
 kind: Config
 preferences: {}
 users:
-- name: ${module.eks.cluster_name}
+- name: ${aws_eks_cluster.eks.name}
   user:
     exec:
       apiVersion: client.authentication.k8s.io/v1beta1
@@ -40,6 +36,6 @@ users:
       - eks
       - get-token
       - --cluster-name
-      - ${module.eks.cluster_name}
+      - ${aws_eks_cluster.eks.name}
 EOF
 }
